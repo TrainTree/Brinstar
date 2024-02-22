1661ba341fe159c7b3a703fc1eb911ab62d6409d
Audibletoaster <110912021+TrainTree@users.noreply.github.com>
2024-02-22 21:16:23 +1100

    Update validate.js
    change to raw pword

--- netlify/functions/validate.js
+++ netlify/functions/validate.js
@@ -1,8 +1,8 @@
 exports.handler = async (event, context) => {
   const { password } = JSON.parse(event.body);
-  const hashedPassword = "b9af17af4fc5708e9ba636512213b9d0cf6f894c0c3ba6e1d970c13a921c5db9"; // Replace with your hashed password
+  const rawPassword = "your_raw_password"; // Set your raw password here
 
-  if (password === hashedPassword) {
+  if (password === rawPassword) {
     return {
       statusCode: 200,
       headers: {
