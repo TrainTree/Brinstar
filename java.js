b4f9c063965dca7fac499b3d3e9415ee194584f1
Audibletoaster <110912021+TrainTree@users.noreply.github.com>
2024-02-22 19:58:58 +1100

    Create java.js

---
+++ java.js
@@ -0,0 +1,21 @@
+// validate.js
+
+exports.handler = async (event, context) => {
+  const { password } = JSON.parse(event.body);
+  const hashedPassword = "b9af17af4fc5708e9ba636512213b9d0cf6f894c0c3ba6e1d970c13a921c5db9"; // Replace with your hashed password
+  
+  if (password === hashedPassword) {
+    return {
+      statusCode: 302, // Redirect status code
+      headers: {
+        Location: '/01 Index.md', // Redirect location
+      },
+      body: '',
+    };
+  } else {
+    return {
+      statusCode: 401, // Unauthorized status code
+      body: 'Incorrect password. Please try again.',
+    };
+  }
+};
