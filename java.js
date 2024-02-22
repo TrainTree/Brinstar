0013074c723ec0feebb80d76b998636fcdb33c92
Audibletoaster <110912021+TrainTree@users.noreply.github.com>
2024-02-22 20:24:18 +1100

    Update java.js
    GTP validate fix

--- java.js
+++ java.js
@@ -1,25 +1,16 @@
-document.getElementById('password-form').addEventListener('submit', async function(event) {
-  event.preventDefault(); // Prevent the form from submitting normally
+exports.handler = async (event, context) => {
+  const { password } = JSON.parse(event.body);
+  const hashedPassword = "b9af17af4fc5708e9ba636512213b9d0cf6f894c0c3ba6e1d970c13a921c5db9"; // Replace with your hashed password
 
-  const password = document.getElementById('password-input').value;
-
-  try {
-    const response = await fetch('/.netlify/functions/validate', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ password })
-    });
-
-    if (response.ok) {
-      const data = await response.json();
-      window.location.href = data.redirectUrl; // Redirect to the specified URL
-    } else {
-      const errorMessage = await response.text();
-      alert(errorMessage); // Display error message
-    }
-  } catch (error) {
-    console.error('Error:', error);
+  if (password === hashedPassword) {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ redirectUrl: 'https://github.com/TrainTree/Brinstar/blob/main/index.html' }) // Redirect location
+    };
+  } else {
+    return {
+      statusCode: 401,
+      body: 'Incorrect password. Please try again.'
+    };
   }
-});
+};
